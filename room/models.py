4ccf17204852bb35490c55caefce0199d8a4360f
Justin Hwang <j.hwang@iwoca.co.uk>
2022-05-17 09:29:36 +0100

    Create room models

--- room/models.py
+++ room/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField("Name", max_length=1000)
+    is_public = models.BooleanField("Is public")
+    password = models.CharField("Password", max_length=50, blank=True, null=True)
+    capacity = models.IntegerField("Capacity")
+
+    def __str__(self):
+        return self.name
